770b65f705122349137105eed1aaa20aa4289c70
BonifaceMaina <karisbm@gmail.com>
2017-11-22 01:15:29 +0300

    (Tests) Add tests

--- test_events.py
+++ test_events.py
@@ -8,8 +8,33 @@ class ViewTesting(unittest.TestCase):
         resp = client.get('/', content_type='html/text')
         self.assertEqual(resp.status_code, 200)
 
+
+    def test_logout_redirects_to_login(self):
+        client = app.test_client(self)
+        resp = client.get('/logout', follow_redirects=True)
+        self.assertEqual(resp.status_code, 200)
+
+    def test_dashboard_redirects_to_event(self):
+        client = app.test_client(self)
+        resp = client.get('/dashboard', follow_redirects=True)
+        self.assertEqual(resp.status_code, 200)
+
+
+
 class UserTesting(unittest.TestCase):
     """Testing user cases"""
+    def setUp(self):
+        User.users = {}
+        self.app = User('john', 'john@gmail.com', 'pass123' )
+        self.default_user_data = {
+            1:{
+                'name': 'john',
+                'email': 'john@gmail.com',
+                'password': 'pass123'
+            }
+        }
+
+
     def test_registration(self):
         """test user registration"""
         client = app.test_client(self)
